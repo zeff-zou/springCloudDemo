490884d3813aca103ca2e0446b13ed89348e6bea
zouzhefu <371244694@qq.com>
2017-03-07 18:21:14 +0800

    简单实现RabbitMQ

---
+++ rabbitmq/src/main/java/com/clouddemo/rabbit/Sender.java
@@ -0,0 +1,21 @@
+package com.clouddemo.rabbit;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+/**
+ * Created by zzf on 2017/3/7.
+ */
+@Component
+public class Sender {
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void send() {
+        String context = "hello " + new Date();
+        System.out.println("Sender : " + context);
+        this.rabbitTemplate.convertAndSend("hello", context);
+    }
+}
--- rabbitmq/src/main/java/com/clouddemo/web/RabbitmqController.java
+++ rabbitmq/src/main/java/com/clouddemo/web/RabbitmqController.java
@@ -1,5 +1,7 @@
 package com.clouddemo.web;
 
+import com.clouddemo.rabbit.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class RabbitmqController {
+    @Autowired
+    private Sender sender;
     @RequestMapping("/hello")
     public String index() {
+        sender.send();
         return "Hello World";
     }
 }
